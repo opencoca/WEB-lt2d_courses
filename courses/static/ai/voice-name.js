8d11494784f3db71114f1fcfb17e1a7a8ccc8b41
OpenCo <admin@openco.ca>
2020-12-11 20:26:15 -0500

    Cleaned up Snap loading time :D

--- courses/static/ai/voice-name.js
+++
@@ -1,6 +0,0 @@
-var voices = window.speechSynthesis.getVoices();
-if (voice >= 0 && voice < voices.length) {
-    return voices[Math.floor(voice)].name;
-} else {
-    alert("Only " + voices.length + " voices are available. You can't choose voice number " + voice);
-}
