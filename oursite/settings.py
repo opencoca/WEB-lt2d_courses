adba24f37069245d13f22c64deedea68e09413d7
Alexander Somma <admin@openco.ca>
2021-01-14 14:37:31 -0500

    Allowing iframe embeding

--- oursite/settings.py
+++ oursite/settings.py
@@ -61,8 +61,10 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+#    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+
 
 ROOT_URLCONF = 'oursite.urls'
 
